96ab9bb050e84116176fb9a9fb75b980ee65ec83
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2018-06-03 01:25:45 +0600

    Added basic github models and migrations

--- github/models.py
+++ github/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class GithubUser(models.Model):
+    name = models.CharField(max_length=100)
+    handler = models.CharField(max_length=50)
+
+
+class Repository(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class PullRequest(models.Model):
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    author = models.ForeignKey(GithubUser,
+                               related_name="authored_prs",
+                               related_query_name="authored_pr",
+                               on_delete=models.CASCADE)
+    merger = models.ForeignKey(GithubUser,
+                               related_name="merged_prs",
+                               related_query_name="merged_pr",
+                               on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+    merged_at = models.DateTimeField()
+    lines_added = models.IntegerField()
+    lines_removed = models.IntegerField()
--- team_analytics/settings.py
+++ team_analytics/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'github.apps.GithubConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
